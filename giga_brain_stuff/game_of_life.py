ba9e1963ccdf0931056ee83d55723bdf1e5391dd
space monkey <yashcgosavi@gmail.com>
2023-05-17 19:07:38 +0530

    GAME OF LIFE DONE

--- giga_brain_stuff/game_of_life.py
+++ giga_brain_stuff/game_of_life.py
@@ -3,9 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-plt.rcParams["figure.autolayout"] = True
 
-fig = plt.figure(figsize=(7, 7))
 dimension = (40, 40)
 first = np.random.randint(2, size=dimension)
 
@@ -21,20 +19,12 @@ def next_gen(old):
     for rows in range(dimension[0]):
         for cols in range(dimension[1]):
             state = old[rows][cols]
-            if (
-                rows == 0
-                or rows == dimension[0] - 1
-                or cols == 0
-                or cols == dimension[1] - 1
-            ):
-                new[rows][cols] = state
-                continue
-            else:
-                sum = count_neighbours(old, rows, cols)
-                if sum == 3 and state == 0:
-                    state = 1
-                elif sum < 2 or sum > 3 and state == 1:
-                    state = 0
+
+            sum = count_neighbours(old, rows, cols)
+            if sum == 3 and state == 0:
+                state = 1
+            elif sum < 2 or sum > 3 and state == 1:
+                state = 0
 
             new[rows][cols] = state
     old[:] = new[:]
@@ -45,7 +35,9 @@ def count_neighbours(old, i, j):
     sum = 0
     for x in range(-1, 2):
         for y in range(-1, 2):
-            sum += old[i + x][j + y]
+            sum += old[(i + x + dimension[0]) % dimension[0]][
+                (j + y + dimension[1]) % dimension[1]
+            ]
     sum -= old[i][j]
     return sum
 
@@ -53,14 +45,14 @@ def count_neighbours(old, i, j):
 if __name__ == "__main__":
     grid = first
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation="nearest")
+    img = ax.imshow(grid, interpolation="nearest", cmap="gray")
+    plt.axis("off")
 
     update_interval = 50
     anim = animation.FuncAnimation(
-        fig,
-        animate,
-        fargs=(img, grid),
-        frames=2000,
-        repeat=True,
+        fig, animate, fargs=(img, grid), frames=50, interval=50, save_count=50
     )
+    f = r"C:/Users/Yash PC/OneDrive/Desktop/animation.gif"
+    writergif = animation.PillowWriter(fps=30)
+    anim.save(f, writer=writergif)
     plt.show()
