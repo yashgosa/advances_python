5cc17c77e76d69b22e183c7d16f43f292f121ac9
space monkey <yashcgosavi@gmail.com>
2023-05-17 17:47:05 +0530

    Game of life with still edges

--- giga_brain_stuff/game_of_life.py
+++ giga_brain_stuff/game_of_life.py
@@ -5,21 +5,62 @@ from matplotlib import animation
 
 plt.rcParams["figure.autolayout"] = True
 
-fig = plt.figure()
-dimension = (10, 10)
+fig = plt.figure(figsize=(7, 7))
+dimension = (40, 40)
+first = np.random.randint(2, size=dimension)
 
 
-def make_arr(dimension):
-    return np.random.randint(2, size=dimension)
+def animate(frameNum, img, grid):
+    next = next_gen(grid)
+    img.set_data(next)
+    return img
 
 
-def animate(i):
-    grid = make_arr(dimension)
-    sns.heatmap(
-        grid, vmax=0.8, cbar=False, xticklabels=False, yticklabels=False, cmap="Greys"
-    )
+def next_gen(old):
+    new = np.zeros(dimension)
+    for rows in range(dimension[0]):
+        for cols in range(dimension[1]):
+            state = old[rows][cols]
+            if (
+                rows == 0
+                or rows == dimension[0] - 1
+                or cols == 0
+                or cols == dimension[1] - 1
+            ):
+                new[rows][cols] = state
+                continue
+            else:
+                sum = count_neighbours(old, rows, cols)
+                if sum == 3 and state == 0:
+                    state = 1
+                elif sum < 2 or sum > 3 and state == 1:
+                    state = 0
+
+            new[rows][cols] = state
+    old[:] = new[:]
+    return new
+
+
+def count_neighbours(old, i, j):
+    sum = 0
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            sum += old[i + x][j + y]
+    sum -= old[i][j]
+    return sum
 
 
 if __name__ == "__main__":
-    anim = animation.FuncAnimation(fig, animate, frames=2000, repeat=True)
+    grid = first
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation="nearest")
+
+    update_interval = 50
+    anim = animation.FuncAnimation(
+        fig,
+        animate,
+        fargs=(img, grid),
+        frames=2000,
+        repeat=True,
+    )
     plt.show()
